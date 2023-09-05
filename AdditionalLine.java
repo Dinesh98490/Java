e9262adecb1e237cd57e2c2e216e76d702eb0aa7
Dinesh 7 <kunwardinesh028@gmail.com>
2023-09-05 23:00:01 +0545

    first service

---
+++ AdditionalLine.java
@@ -0,0 +1,6 @@
+public class AdditionalLine {
+    public static void main(String[] args) {
+        System.out.println("This is \n my first program");
+        System.out.println("I am on the\n module");
+    }
+}
---
+++ MultiLine.java
@@ -0,0 +1,31 @@
+public class MultiLine {
+    public static void main(String[] args) {
+
+        // insert a double quote in the string
+        String a = " A \"quoted\" string is";
+        System.out.println(a);
+        System.out.println(" 'much' better if you learn");
+
+        String b = "the rules of \"escape sequences.\"";
+        System.out.println(b);
+
+        String c =" Also, \"\"represents an empty";
+        System.out.println(c);
+
+        String d = "string. Don't forget: use \"";
+        System.out.println(d);
+
+        String e = "Insted of \"";
+        System.out.println(e);
+
+        String f = "\" is not the same as!";
+        System.out.println(f);
+
+
+
+
+
+
+    }
+    
+}
